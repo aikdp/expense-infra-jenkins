4d595bebc90c082b51a345c70426fc921547b6f2
aikdp <kdprasad028@gmail.com>
2025-02-01 12:56:41 +0530

    jenkins

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {  #need to create manually in AWS, then use here
         bucket         	   = "vpc-module-rs"
-        key                = "expense-vpc-pipeline"
+        key                = "expense-vpc"
         region         	   = "us-east-1"
         dynamodb_table = "vpc-module-locking"
     }
